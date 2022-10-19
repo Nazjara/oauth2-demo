5975cd7f80dcb5a847a859425f8e79238f754635
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2022-10-19 19:58:12 +0300

    Configure scope/role based security

---
+++ src/main/java/com/nazjara/configuration/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.nazjara.configuration;
+
+import com.nazjara.converter.RoleConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfiguration
+{
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new RoleConverter());
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .antMatchers(HttpMethod.GET, "/user/status/check")
+                        .hasAnyAuthority("ROLE_developer", "SCOPE_profile")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
+
+        return http.build();
+    }
+}
