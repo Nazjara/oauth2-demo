e2ea133920fd9a2effa312ffb8a6fd44102056e2
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2022-11-08 17:51:12 +0200

    Add JS client Add CORS configuration in API Gateway

---
+++ js-client/src/main/java/com/nazjara/JsClientApplication.java
@@ -0,0 +1,13 @@
+package com.nazjara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsClientApplication.class, args);
+	}
+
+}
